6ffe93e01836c32611efe30a95ea29b7dc7f13d5
sreecharan-desu <sreecharan309@gmail.com>
2024-08-28 22:49:40 +0530

    added tailwind.config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
-
+import "./index.css";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, I am Tailwind CSS!
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
